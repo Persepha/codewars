842da31ecaa7ad16162b01a93d0a0a4bc28c8a25
Dmitry Vasilevich <vasilevich.dmt@gmail.com>
2021-04-06 23:20:18 +0300

    Add Magic The Gathering #1: Creatures solution

---
+++ python/6-kyu/Magic-The-Gathering-1-Creatures/mtg1_creatures.py
@@ -0,0 +1,9 @@
+def  battle(player1, player2):
+    min_deck = len(min([player1, player2], key=len))
+
+    return {
+        'player1': [creature for i, creature in enumerate(player1)
+                    if i+1 > min_deck or creature[1] > player2[i][0]],
+        'player2': [creature for i, creature in enumerate(player2)
+                    if i+1 > min_deck or creature[1] > player1[i][0]],
+    }
